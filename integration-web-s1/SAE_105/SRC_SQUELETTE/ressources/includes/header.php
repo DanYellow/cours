4d82cca09eb79d2b842d92d4a65edf709c576d81
Michele <michele.linardi@orange.fr>
2021-11-26 16:52:53 +0100

    UPDATE cours avec SAE 105

---
+++ integration-web-s1/SAE_105/SRC_SQUELETTE/ressources/includes/header.php
@@ -0,0 +1,24 @@
+<nav class="navigation-principale">
+    <ul>
+        <li>ACCUEIL</li>
+        <li>A PROPOS</li>
+        <li>CONTACT</li>
+    </ul>
+</nav>
+
+<header class="bulle">
+    <article class="titre">
+        <h1 class="txt-grand">
+            <span>Bachelor</span>
+            <span>Universitaire de</span>
+            <span>Technologie</span>
+        </h1>
+        <h2 class="txt-petit">
+            <span>Métiers du Multimédia et de</span>
+            <span>l'Internet</span>
+        </h2>
+    </article>
+
+    <article class="bulle-icone rose"></article>
+    <article class="bulle-icone-bordure rose"></article>
+</header>
