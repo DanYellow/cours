0bf058704a5c212deb50515242b0334a0aae0884
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2022-01-21 09:12:28 +0100

    Add new files

---
+++ integration-web-s2/en-route-vers-sae-203/ressources/includes/details-patisserie-liste.php
@@ -0,0 +1,15 @@
+<li class='patisserie'>
+    <figure>
+        <img src='ressources/images/patisseries/financiers.jpg' alt=''>
+    </figure>
+    <section class='details'>
+        <section class='textes'>
+            <p class='titre'>Madeleines</p>
+            <p class='prix'>8 euros la demi-douzaine</p>
+        </section>
+        <section class='liste-boutons'>
+            <button type='button' class='cta-primaire'>Ajouter au panier</button>
+            <a href='' class='cta-secondaire'>Voir plus d'infos</a>
+        </section>
+    </section>
+</li>
