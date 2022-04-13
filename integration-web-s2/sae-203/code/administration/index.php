490d1a99e92893bb9df5bcc48613bce97945ad87
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2022-04-13 10:43:16 +0200

    sae 203 remove redirection on administration/

--- integration-web-s2/sae-203/code/administration/index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// A modifier SI ET SEULEMENT SI 
-// vous souhaitez que l'utilisateur arrive sur une autre page 
-// lorsqu'il arrive sur la page "/administration"
-if (substr($_SERVER['REQUEST_URI'], -1) === "/") {
-    header('Location: ./auteurs');
-} else {
-    header('Location: ./administration/auteurs');
-}
