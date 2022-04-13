bfcc25de8405b6485fda18dbe9f29af17ee81d3f
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2022-04-13 09:55:45 +0200

    Fix redirection to administration on live server

--- integration-web-s2/sae-203/code/administration/index.php
+++ integration-web-s2/sae-203/code/administration/index.php
@@ -2,4 +2,14 @@
 // A modifier SI ET SEULEMENT SI 
 // vous souhaitez que l'utilisateur arrive sur une autre page 
 // lorsqu'il arrive sur la page "/administration"
-header('Location: ./administration/auteurs');
+
+$listDomaineLocaux = array(
+    '127.0.0.1',
+    '::1'
+);
+
+if (in_array($_SERVER['REMOTE_ADDR'], $listDomaineLocaux)) {
+    header('Location: ./administration/auteurs');
+} else {
+    header('Location: ./auteurs');
+}
