578059e5d2d9d2d1538c7fb3ee5388596a7c69e6
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2022-07-22 12:58:05 +0200

    Update

--- scripts.js
+++ scripts.js
@@ -1,27 +1,42 @@
 const initTabSystem = () => {
-    const openTab = (e) => {
-        document.querySelectorAll("[data-tab-content]").forEach((item) => {
-            item.style.display = "none";
-        });
-
-        document.querySelectorAll("[data-tab-name]").forEach((item) => {
-            item.classList.remove("active");
-        });
-
-        document.querySelector(
-            `[data-tab-content="${e.target.dataset.tabName}"]`
-        ).style.display = "block";
-        document
-            .querySelector(`[data-tab-name="${e.target.dataset.tabName}"]`)
-            .classList.add("active");
-    };
-
-    document.querySelectorAll("[data-tab-content]")[0].style.display = "block";
-    document.querySelectorAll("[data-tab-name]")[0].classList.add("active")
+  const openTab = (e) => {
+    document.querySelectorAll("[data-tab-content]").forEach((item) => {
+      item.style.display = "none";
+    });
 
     document.querySelectorAll("[data-tab-name]").forEach((item) => {
-        item.addEventListener("click", openTab);
+      item.classList.remove("active");
     });
+
+    document.querySelector(
+      `[data-tab-content="${e.target.dataset.tabName}"]`
+    ).style.display = "block";
+    document
+      .querySelector(`[data-tab-name="${e.target.dataset.tabName}"]`)
+      .classList.add("active");
+  };
+
+  if (document.querySelectorAll("[data-tab-content]").length) {
+    document.querySelectorAll("[data-tab-content]")[0].style.display = "block";
+    document.querySelectorAll("[data-tab-name]")[0].classList.add("active");
+  }
+
+  document.querySelectorAll("[data-tab-name]").forEach((item) => {
+    item.addEventListener("click", openTab);
+  });
 };
 
-initTabSystem()
+initTabSystem();
+
+const initAccordionSystem = () => {
+  var url = new URL(window.location.href);
+  var params = new URLSearchParams(url.search);
+
+  const accordionIndex = Number(params.get("a"))
+  if(accordionIndex) {
+    document.querySelectorAll("[data-instructions-container]")[accordionIndex].open = true
+    
+  }
+};
+
+initAccordionSystem();
