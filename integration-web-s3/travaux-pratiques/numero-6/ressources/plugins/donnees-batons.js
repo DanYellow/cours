0061638ba29c272ec1266f3197525c38e891311b
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-08-06 00:15:47 +0200

    add course for js plugins

---
+++ integration-web-s3/travaux-pratiques/numero-6/ressources/plugins/donnees-batons.js
@@ -0,0 +1,8 @@
+const prenomAnthony2005 = [
+  { preusuel: 'ANTHONY', dpt: 75, nombre: 26 },
+  { preusuel: 'ANTHONY', dpt: 78, nombre: 20 },
+  { preusuel: 'ANTHONY', dpt: 91, nombre: 11 },
+  { preusuel: 'ANTHONY', dpt: 92, nombre: 24 },
+  { preusuel: 'ANTHONY', dpt: 93, nombre: 4 },
+  { preusuel: 'ANTHONY', dpt: 95, nombre: 8 },
+]
