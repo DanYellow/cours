c23506aeac96f28813f55cc5e3416a28bde92891
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-08-08 16:20:31 +0200

    Add course on javascript plugins

--- integration-web-s3/travaux-pratiques/numero-6/ressources/plugins/donnees-batons.js
+++ integration-web-s3/travaux-pratiques/numero-6/ressources/plugins/donnees-batons.js
@@ -1,8 +1,10 @@
 const prenomAnthony2005 = [
-  { preusuel: 'ANTHONY', dpt: 75, nombre: 26 },
-  { preusuel: 'ANTHONY', dpt: 78, nombre: 20 },
-  { preusuel: 'ANTHONY', dpt: 91, nombre: 11 },
-  { preusuel: 'ANTHONY', dpt: 92, nombre: 24 },
-  { preusuel: 'ANTHONY', dpt: 93, nombre: 4 },
-  { preusuel: 'ANTHONY', dpt: 95, nombre: 8 },
+  { preusuel: 'JEROME', dpt: '60', nombre: 2144 },
+  { preusuel: 'JEROME', dpt: '75', nombre: 8017 },
+  { preusuel: 'JEROME', dpt: '77', nombre: 2193 },
+  { preusuel: 'JEROME', dpt: '78', nombre: 3593 },
+  { preusuel: 'JEROME', dpt: '91', nombre: 1907 },
+  { preusuel: 'JEROME', dpt: '92', nombre: 3558 },
+  { preusuel: 'JEROME', dpt: '93', nombre: 3013 },
+  { preusuel: 'JEROME', dpt: '95', nombre: 2099 },
 ]
