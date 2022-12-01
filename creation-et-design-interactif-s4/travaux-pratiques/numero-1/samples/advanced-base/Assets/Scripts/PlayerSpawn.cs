3f95705ce740e208d46b945c34f7390b999e9949
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-12-01 15:33:04 +0100

    Update manifest

--- creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/PlayerSpawn.cs
+++ creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/PlayerSpawn.cs
@@ -5,7 +5,7 @@ public class PlayerSpawn : MonoBehaviour
     [Tooltip("Define where the player will spawn if there is an issue")]
     public Vector3 currentSpawnPosition;
 
-    [Tooltip("Define where the player started the game")]
+    [Tooltip("Define where the player started the level")]
     public Vector3 initialSpawnPosition;
     private void Awake()
     {
