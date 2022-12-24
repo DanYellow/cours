295e5568050b4ad8e3cbcc0e09169d29c842dc6a
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-12-24 16:43:14 +0100

    Disable controls on knockback

--- creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
+++ creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Knockback : MonoBehaviour
 {
@@ -6,6 +7,17 @@ public class Knockback : MonoBehaviour
 
     public void Knockbacked(Vector3 direction, float strength)
     {
-        rb.MovePosition(transform.position + direction * strength);       
+        rb.MovePosition(transform.position + direction * strength);
+        StartCoroutine(DisableControls());
+    }
+
+    IEnumerator DisableControls()
+    {
+        if (TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        {
+            playerMovement.enabled = false;
+            yield return new WaitForSeconds(0.21f);
+            playerMovement.enabled = true;
+        }
     }
 }
