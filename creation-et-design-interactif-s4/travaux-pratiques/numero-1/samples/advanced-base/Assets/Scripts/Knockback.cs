eb32ecad09eb2a56506c9c1dd4ff7e3f11c51356
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-12-25 19:08:17 +0100

    Remove shake on knockback

--- creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
+++ creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
@@ -4,30 +4,11 @@ using System.Collections;
 public class Knockback : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public ShakeTypeVariable shakeType = null;
 
     public void Knockbacked(Vector3 direction, float strength)
     {
         rb.MovePosition(transform.position + direction * strength);
         StartCoroutine(DisableControls());
-        StartCoroutine(Shake(shakeType.Duration, shakeType.Magnitude));
-    }
-
-    private IEnumerator Shake(float duration, float magnitude)
-    {
-        Vector3 orignalPosition = transform.position;
-        float elapsed = 0f;
-
-        while (elapsed < duration)
-        {
-            Vector3 shakeVector = (Random.insideUnitSphere / 2) * magnitude;
-
-            transform.position = new Vector3(
-                shakeVector.x + orignalPosition.x, shakeVector.y + orignalPosition.y, orignalPosition.z);
-            elapsed += Time.deltaTime;
-            yield return 0;
-        }
-        transform.position = orignalPosition;
     }
 
     IEnumerator DisableControls()
