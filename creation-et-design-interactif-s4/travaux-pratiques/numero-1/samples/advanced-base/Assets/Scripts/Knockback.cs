89650cb93e35b45717a50e067b6f4c3285fcad1e
PC-Dan <jeanlouis.danielo@sfr.fr>
2022-12-25 19:03:09 +0100

    Add shake on knockback

--- creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
+++ creation-et-design-interactif-s4/travaux-pratiques/numero-1/samples/advanced-base/Assets/Scripts/Knockback.cs
@@ -4,11 +4,30 @@ using System.Collections;
 public class Knockback : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public ShakeTypeVariable shakeType = null;
 
     public void Knockbacked(Vector3 direction, float strength)
     {
         rb.MovePosition(transform.position + direction * strength);
         StartCoroutine(DisableControls());
+        StartCoroutine(Shake(shakeType.Duration, shakeType.Magnitude));
+    }
+
+    private IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 orignalPosition = transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            Vector3 shakeVector = (Random.insideUnitSphere / 2) * magnitude;
+
+            transform.position = new Vector3(
+                shakeVector.x + orignalPosition.x, shakeVector.y + orignalPosition.y, orignalPosition.z);
+            elapsed += Time.deltaTime;
+            yield return 0;
+        }
+        transform.position = orignalPosition;
     }
 
     IEnumerator DisableControls()
@@ -16,7 +35,7 @@ public class Knockback : MonoBehaviour
         if (TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
         {
             playerMovement.enabled = false;
-            yield return new WaitForSeconds(0.21f);
+            yield return new WaitForSeconds(0.20f);
             playerMovement.enabled = true;
         }
     }
