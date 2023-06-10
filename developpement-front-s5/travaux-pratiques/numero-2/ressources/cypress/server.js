743edfb1a0954bcdfc87c1bedd33f75ed1d77e5b
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-06-10 09:58:35 +0200

    wip

--- developpement-front-s5/travaux-pratiques/numero-2/ressources/cypress/server.js
+++ developpement-front-s5/travaux-pratiques/numero-2/ressources/cypress/server.js
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 }); 
 
 app.listen(3000, () => {
-  console.log("Server is running on http::/localhost:3000");
+  console.log("Server is running on http://localhost:3000");
 });
--- developpement-front-s5/travaux-pratiques/numero-2/ressources/cypress/src/script.js
+++ developpement-front-s5/travaux-pratiques/numero-2/ressources/cypress/src/script.js
@@ -1,24 +1,28 @@
+import { getListNationalHolidays } from "./api.js";
+
 const banner = document.querySelector(".banner")
 
 const parcoursFullName = {
     "dev": "développement web et dispositifs interactifs",
     "crea": "création numérique",
     "strat": "stratégie de communication numérique et design d’expérience"
-}
+};
 
 document
     .querySelector("[data-form]")
-    .addEventListener("submit", (e) => {
+    .addEventListener("submit", async (e) => {
         e.preventDefault()
-
+        
         const formData = new FormData(e.target);
         const formValues = Object.fromEntries(formData.entries())
         const bannerTextTpl = banner.querySelector('p').innerHTML;
-
+        
         banner.querySelector('p').innerHTML = bannerTextTpl.replace("__parcours_placeholder__", parcoursFullName[formValues.parcours])
         banner.classList.remove("hidden")
         
         document.querySelector('a').classList.remove("pointer-events-none", "opacity-50")
+        
+        await getListNationalHolidays()
         
         Array.from(e.target.elements)
             .forEach((item) => {
