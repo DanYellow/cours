aa20a51b074c120f481b653136c43234bee8ad9d
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2023-07-04 17:38:53 +0200

    SAE-402 - Update ObjectPooling script

--- creation-et-design-interactif-s4/samples/advanced-base/Assets/Scripts/Utils/ObjectPooling.cs
+++ creation-et-design-interactif-s4/samples/advanced-base/Assets/Scripts/Utils/ObjectPooling.cs
@@ -5,27 +5,53 @@ using System.Collections.Generic;
 // https://gameprogrammingpatterns.com/object-pool.html
 public class ObjectPooling : MonoBehaviour
 {
-
     [SerializeField]
-    private GameObject objectToPool;
+    private GameObject prefab;
 
     [SerializeField]
     private int poolSize = 5;
-    private Queue<GameObject> listObjectPooled = new Queue<GameObject>();
+    private Queue<GameObject> queueObjectsPooled = new Queue<GameObject>();
 
     public Transform container;
 
     public void Initialize(GameObject _objectToPool, int _poolSize)
     {
-        objectToPool = _objectToPool;
+        prefab = _objectToPool;
         poolSize = _poolSize;
     }
 
-    private void CreateContainerIfNeeded()
+    public GameObject CreateObject()
     {
-        if (container != null)
+        GameObject poolObject = null;
+
+        if (queueObjectsPooled.Count < poolSize)
         {
-            string containerName = $"ContainerPool_{objectToP}"
+            poolObject = Instantiate(prefab, transform.position, Quaternion.identity);
+            poolObject.name = $"{transform.root.name}_{objectToPool.name}_{queueObjectsPooled.Count}";
+            // poolObject.SetActive(false);
+        } else {
+            poolObject = queueObjectsPooled.Dequeue();
+            poolObject.transform.position = transform.position;
+            poolObject.transform.rotation = Quaternion.identity;
+            poolObject.SetActive(true);
         }
+
+        queueObjectsPooled.Enqueue(poolObject);
+
+        return poolObject;
     }
+
+    // private void CreateContainerIfNeeded()
+    // {
+    //     if (container != null)
+    //     {
+    //         string containerName = $"ContainerPool_{objectToPool.name}";
+    //         GameObject containerGO = GameObject.Find(containerName);
+    //         if (containerGO != null) {
+    //             container = containerGO.transform;
+    //         } else {
+                
+    //         }
+    //     }
+    // }
 }
