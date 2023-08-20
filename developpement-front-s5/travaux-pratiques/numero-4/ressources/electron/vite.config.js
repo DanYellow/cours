d94ad6b085048ee54dabb69aa37bd4b0dd71c0fe
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-08-20 19:41:22 +0200

    dev-front-s5 - Clear vite.config + add csp slides

--- developpement-front-s5/travaux-pratiques/numero-4/ressources/electron/vite.config.js
+++ developpement-front-s5/travaux-pratiques/numero-4/ressources/electron/vite.config.js
@@ -1,11 +1,7 @@
 import path from "path";
-import { spawn } from "child_process";
 
-import electron from "electron";
 import { defineConfig } from "vite";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-
-spawn(electron, ["."]);
 
 export default defineConfig(() => {
   return {
@@ -21,27 +17,6 @@ export default defineConfig(() => {
       }),
     ],
     root: path.resolve(__dirname, 'src'),
-    publicDir: false,
-    build: {
-      emptyOutDir: true,
-      target: "esnext",
-      outDir: "dist",
-      watch: {
-        buildDelay: 350,
-        exclude: ["node_modules/**", "main.js", "vite.config.js"],
-      },
-      rollupOptions: {
-        input: {
-          // nom du fichier compilé: nom du fichier original
-          preload: "preload.js",
-          renderer: "script.js",
-        },
-        output: {
-          entryFileNames: "[name].js",
-          assetFileNames: "[name].[ext]",
-        },
-      },
-    },
     server: {
       port: 9117,
       open: false, 
