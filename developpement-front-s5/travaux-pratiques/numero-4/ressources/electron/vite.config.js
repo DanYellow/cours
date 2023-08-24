055672a38da0467875488586ea9385f0c8c6bbda
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-08-24 13:36:22 +0200

    dev-front-s5 - Fix errors about CSP-Report-Only

--- developpement-front-s5/travaux-pratiques/numero-4/ressources/electron/vite.config.js
+++ developpement-front-s5/travaux-pratiques/numero-4/ressources/electron/vite.config.js
@@ -21,10 +21,10 @@ export default defineConfig(() => {
       port: 7777,
       open: false,
       headers: {
-        "Content-Security-Policy-Report-Only": "default-src 'self' https://www.youtube.com/; report-uri http://localhost:8000/csp-report.php;",
+        "Content-Security-Policy-Report-Only": "default-src 'self' https://www.youtube.com/; report-uri mon.url;",
         // OU (plus moderne) - Ne fonctionne qu'en HTTPS !
         // "Content-Security-Policy-Report-Only": "default-src 'self' https://www.youtube.com/; report-to endpoint"
-        // "Reporting-Endpoints": "endpoint="https://localhost:8000/csp-report.php",
+        // "Report-To": '{"group":"endpoint","max_age":31536000,"endpoints":[{"url":" mon.url"}]}',
       }
     },
   };
