0acd1d0a0a5a583044b6edbd603aa2dc8f139048
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-10-02 23:13:44 +0200

    developpement-front-s5 - Update instruction tp#4

--- developpement-front-s5/travaux-pratiques/numero-4/ressources/vite-config/vite.config.js
+++ developpement-front-s5/travaux-pratiques/numero-4/ressources/vite-config/vite.config.js
@@ -1,21 +1,18 @@
-import { loadEnv } from "vite";
 import twig from "@vituum/vite-plugin-twig";
 import vituum from "vituum";
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [
-      vituum(),
-      twig({
-        // Where the twig files are located
-        root: "./src",
-      }),
-    ],
-    server: {
-      // Port of the server
-      port: 9117,
-    },
-  };
+export default {
+  plugins: [
+    vituum(),
+    twig({
+      // Where the twig files are located
+      root: "./src",
+    }),
+  ],
+  server: {
+    // Port of the server
+    port: 9117,
+    // Expose the server to the network allowing access from ip address
+    host: true,
+  },
 };
