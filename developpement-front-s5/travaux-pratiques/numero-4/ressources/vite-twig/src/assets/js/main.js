73bfbb688a149cf4cf6693ef0a31ff4efc8feed5
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2023-10-18 14:57:10 +0200

    developpement-front-s5 - Update folder structure

--- developpement-front-s5/travaux-pratiques/numero-4/ressources/vite-twig/src/assets/js/main.js
+++
@@ -1,4 +0,0 @@
-import '/src/assets/styles/style.css'
-import '/src/assets/styles/header.css'
-
-console.log(import.meta.env.VITE_HELLO)
---
+++ developpement-front-s5/travaux-pratiques/numero-4/ressources/vite-twig/src/scripts/main.js
@@ -0,0 +1,4 @@
+import '/src/styles/style.css'
+import '/src/styles/header.css'
+
+console.log(import.meta.env.VITE_HELLO)
