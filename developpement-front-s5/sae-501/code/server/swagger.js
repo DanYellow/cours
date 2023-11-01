713250778817609bd8274ce1fb36bba9fbce689b
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-11-01 17:59:41 +0100

    developpement-front-s5 - SAE-501 - Update swagger version

--- developpement-front-s5/sae-501/code/server/swagger.js
+++ developpement-front-s5/sae-501/code/server/swagger.js
@@ -6,6 +6,7 @@ const options = {
     apis: [path.join(path.resolve(), "server/api-router/*.js")],
     swaggerDefinition: {
         basePath: "/api",
+        openapi: '3.0.0',
         produces: ["application/json"],
         info: {
             title: "SAE 501",
