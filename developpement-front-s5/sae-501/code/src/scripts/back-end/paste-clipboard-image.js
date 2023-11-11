47f26b438063f63aaebc2a5e43c9359692ee6b04
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-11-11 15:46:17 +0100

    developpement-front-s5 - SAE-501 - Manage clipboard permission

--- developpement-front-s5/sae-501/code/src/scripts/back-end/paste-clipboard-image.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/paste-clipboard-image.js
@@ -1,31 +1,51 @@
 const listClipboardUploadButton = document.querySelectorAll("[data-clipboard-upload]")
 
-const pasteClipboard = async (e) => {
-    const element = e.currentTarget
-    const name = element.dataset.clipboardUpload
+const pasteClipboard = async (e) => {   
+    const element = e.target;
+    const name = element.dataset.clipboardUpload;
 
-    const clipboardContents = await navigator.clipboard.read();
-
-    const img = clipboardContents.filter((item) => item.types.includes("image/png"))[0]
-    if(img) {
-        const blob = await img.getType("image/png");
-
-        const file = new File([blob], "img.jpg", { type: blob.type, lastModified:new Date().getTime() });
-        const container = new DataTransfer();
-        container.items.add(file);
-
-        const inputFile = document.querySelector(`[data-upload-file="${name}"]`)
-
-        inputFile.files = container.files
-        inputFile.dispatchEvent(new Event('change'))
-    } else {
+    try {
+        const clipboardContents = await navigator.clipboard.read();
+        const img = clipboardContents.filter((item) => item.types.includes("image/png"))[0]
+        if(img) {
+            const blob = await img.getType("image/png");
+    
+            const file = new File([blob], "img.jpg", { type: blob.type, lastModified:new Date().getTime() });
+            const container = new DataTransfer();
+            container.items.add(file);
+    
+            const inputFile = document.querySelector(`[data-upload-file="${name}"]`)
+    
+            inputFile.files = container.files
+            inputFile.dispatchEvent(new Event('change'))
+        } else {
+            alert("Image non trouvée dans le presse-papier")
+        }
+    } catch (error) {
         alert("Image non trouvée dans le presse-papier")
     }
 }
 
+const getClipboardPermission = async (e) => {
+    try {
+        const permission = await navigator.permissions.query({
+          name: "clipboard-read",
+        });
+        if (permission.state === "denied") {
+            throw new Error("Permission d'accès au presse-papier refusée");
+        } else {
+            pasteClipboard(e);
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+
 listClipboardUploadButton.forEach((item) => {
-    if(!navigator.clipboard.read) {
+    if(!navigator.clipboard?.read) {
         item.remove()
     }
-    item.addEventListener("click", pasteClipboard)
+
+    item.addEventListener("click", getClipboardPermission)
 })
