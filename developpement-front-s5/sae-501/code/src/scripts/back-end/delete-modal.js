6d32d60d5bfd790d91bc01af338e601367f197c6
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-12-09 20:09:10 +0100

    developpement-front-s5 - SAE-501 - Use popover for delete entity

--- developpement-front-s5/sae-501/code/src/scripts/back-end/delete-modal.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/delete-modal.js
@@ -1,20 +1,17 @@
 import axios from "axios";
-import { createFocusTrap } from 'focus-trap';
+import { createFocusTrap } from "focus-trap";
 
 const deletionModal = document.querySelector("[data-deletion-modal]")
 const closeModalBtn = document.querySelector("[data-deletion-modal] [data-close-modal]")
 const deleteItemModalBtn = document.querySelector("[data-deletion-modal] [data-delete-item]")
 const errorMessageModal = document.querySelector("[data-deletion-modal] [data-error-modal]")
 
-let focusTrap = null
+let focusTrap = null;
 
 const displayDeleteItemModal = async (e) => {
     deletionModal.classList.remove("hidden")
     deleteItemModalBtn.dataset.deleteItem = e.currentTarget.dataset.deleteUrl
     deletionModal.querySelector("[data-modal-item-name]").textContent = e.currentTarget.dataset.deleteName
-
-    focusTrap = createFocusTrap(deletionModal)
-    focusTrap.activate()
 }
 
 closeModalBtn.addEventListener("click", () => {
@@ -44,4 +41,13 @@ deleteItemModalBtn.addEventListener("click", async (e) => {
 
 document.querySelectorAll('[data-delete-url]').forEach((item) => {
     item.addEventListener("click", displayDeleteItemModal)
+})
+
+deletionModal.addEventListener("toggle", (evt) => {
+    if (evt.newState === "open") {
+        focusTrap = createFocusTrap(deletionModal);
+        focusTrap.activate();
+    } else {
+        focusTrap.deactivate()
+    }
 })
