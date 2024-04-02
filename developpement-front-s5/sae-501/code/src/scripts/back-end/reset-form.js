b6f1ef96015b24a3e1df7b5d8ed5c6c8bfe31f87
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-02 19:18:50 +0200

    SAE-501 - Update reset form script

--- developpement-front-s5/sae-501/code/src/scripts/back-end/reset-form.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/reset-form.js
@@ -1,9 +1,8 @@
 const form = document.querySelector("[data-form]")
 
 form?.addEventListener("reset", (e) => {
-    const listImgs = e.target.querySelectorAll("[data-original-image]")
+    const listImgs = e.target.querySelectorAll("[data-preview-upload]")
     listImgs.forEach((item) => {
-        const originalImage = item.dataset.originalImage
-        item.src = originalImage
+        item.src = "";
     })    
 })
