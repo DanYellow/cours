a9bb7bb286a42ea8beeee1ca3adb35bdf2157511
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-03 11:04:33 +0200

    SAE-501 - Reset image already uploaded on reset form

--- developpement-front-s5/sae-501/code/src/scripts/back-end/delete-uploaded-image.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/delete-uploaded-image.js
@@ -1,5 +1,19 @@
 const listDeleteCurrentImageBtn = document.querySelectorAll("[data-delete-current-image-button]");
 
+const imageObserver = new MutationObserver((mutationList) => {
+    mutationList.forEach((mutation) => {
+        if (mutation.type === "attributes") {
+            const deleteBtn = document.querySelector(
+                `[data-delete-current-image-button="${mutation.target.dataset.currentImage}"]`
+            );
+
+            const newValue = mutation.target.getAttribute("src");
+            deleteBtn.classList.toggle("hidden", newValue === "");
+        }
+    });
+});
+
+
 listDeleteCurrentImageBtn.forEach((item) => {
     item.addEventListener("click", (e) => {
         const dataAttr = e.currentTarget.dataset.deleteCurrentImageButton;
@@ -9,6 +23,10 @@ listDeleteCurrentImageBtn.forEach((item) => {
         const img = document.querySelector(`[data-current-image="${dataAttr}"]`);
         img.src = "";
 
-        e.currentTarget.parentNode.removeChild(e.currentTarget);
+        imageObserver.observe(img, {
+            attributes: true,
+        });
+
+        e.currentTarget.classList.add("hidden");
     });
 });
--- developpement-front-s5/sae-501/code/src/scripts/back-end/reset-form.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/reset-form.js
@@ -2,7 +2,13 @@ const form = document.querySelector("[data-form]")
 
 form?.addEventListener("reset", (e) => {
     const listImgs = e.target.querySelectorAll("[data-preview-upload]")
+    const listUploadedImgs = e.target.querySelectorAll("[data-image-path]")
+    
     listImgs.forEach((item) => {
         item.src = "";
-    })    
+    })
+    
+    listUploadedImgs.forEach((item) => {
+        item.src = item.dataset.imagePath;
+    })
 })
