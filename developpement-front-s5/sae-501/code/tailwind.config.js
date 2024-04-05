48b074d5377165888918ffd95f2b6ef6c44c989c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-05 18:55:35 +0200

    SAE-501 - Implement container queries for input file custom

--- developpement-front-s5/sae-501/code/tailwind.config.js
+++ developpement-front-s5/sae-501/code/tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 
+import containerQueries from "@tailwindcss/container-queries";
+import forms from "@tailwindcss/forms";
+
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx,njk}"],
-  theme: {
-    extend: {
-      spacing: {
-        11: "2.75rem",
-        12: "3.25rem",
-        13: "3.75rem",
-        14: "4.25rem",
-      },
+    content: ["./src/**/*.{js,ts,jsx,tsx,njk}"],
+    theme: {
+        extend: {
+            spacing: {
+                11: "2.75rem",
+                12: "3.25rem",
+                13: "3.75rem",
+                14: "4.25rem",
+            },
+            containers: {
+                "5.5xl": "69rem",
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    plugins: [containerQueries, forms],
 };
