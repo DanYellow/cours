ec67792a0a696545e92db146248bf16277c4615c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-12 08:29:37 +0200

    SAE-501 - Use url instead of object query for SAE list on index admin

--- developpement-front-s5/sae-501/code/server/back-end-router/index.js
+++ developpement-front-s5/sae-501/code/server/back-end-router/index.js
@@ -4,8 +4,6 @@ import fs from "fs/promises";
 import axios from "axios";
 import querystring from "querystring";
 
-// Models
-import SAE from "#models/sae.js";
 
 // Routers
 import SAERouter from './sae.js'
@@ -28,7 +26,6 @@ const parseManifest = async () => {
     return JSON.parse(manifestFile);
 };
 
-
 router.use(async (_req, res, next) => {
     const originalRender = res.render;
     res.render = async function (view, local, callback) {
@@ -47,22 +44,24 @@ router.use(SAERouter)
 router.use(articleRouter)
 
 router.get("/", async (_req, res) => {
-    const queryParams = querystring.stringify({ per_page: 5 });
-
-    const listSAEs = await SAE.find().limit(5).sort({ _id: -1 });
-    const countSAEs = await SAE.count();
-
-    let optionsArticles = {
+    const queryParamsSAEs = querystring.stringify({ per_page: 5 });
+    const optionsSAEs = {
         method: "GET",
-        url: `${res.locals.base_url}/api/articles?${queryParams}`,
+        url: `${res.locals.base_url}/api/saes?${queryParamsSAEs}`,
     }
-
+    const listSAEs = await axios(optionsSAEs);
+    
+    const queryParamsArticles = querystring.stringify({ per_page: 5 });
+    const optionsArticles = {
+        method: "GET",
+        url: `${res.locals.base_url}/api/articles?${queryParamsArticles}`,
+    }
     const listArticles = await axios(optionsArticles);
 
     res.render("pages/back-end/index.njk", {
         list_saes: {
-            data: listSAEs,
-            count: countSAEs,
+            data: listSAEs.data.data,
+            count: listSAEs.data.count,
         },
         list_articles: {
             data: listArticles.data.data,
