3e118198d7a41b858a4050339e98a5520889982d
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-06-02 22:12:30 +0200

    SAE-501 - Refactoting requests in sae's admin

--- developpement-front-s5/sae-501/code/server/back-end-router/sae.js
+++ developpement-front-s5/sae-501/code/server/back-end-router/sae.js
@@ -15,7 +15,7 @@ router.get(`/${base}`, async (req, res) => {
     let options = {
         method: "GET",
         url: `${res.locals.base_url}/api/${base}?${queryParams}`,
-    }
+    };
     let result = null;
     try {
         result = await axios(options);
@@ -26,82 +26,86 @@ router.get(`/${base}`, async (req, res) => {
     });
 });
 
-// Get or create SAE
-router.get([`/${base}/:id`, `/${base}/add`], async (req, res) => {
-    let options = {
-        method: "GET",
-        url: `${res.locals.base_url}/api/${base}/${req.params.id}`,
-    }
-    const isEdit = mongoose.Types.ObjectId.isValid(req.params.id);
+router
+    .route([`/${base}/:id`, `/${base}/add`])
+    .get(async (req, res) => {
+        // Get or create SAE
+        let options = {
+            method: "GET",
+            url: `${res.locals.base_url}/api/${base}/${req.params.id}`,
+        };
+        const isEdit = mongoose.Types.ObjectId.isValid(req.params.id);
 
-    let result = null;
-    let listErrors = [];
-    
-    if(isEdit) {
+        let result = null;
+        let listErrors = [];
+
+        if (isEdit) {
+            try {
+                result = await axios(options);
+            } catch (e) {
+                listErrors = e.response.data.errors;
+                result = {};
+            }
+        }
+
+        res.render("pages/back-end/saes/add-edit.njk", {
+            sae: result?.data || {},
+            list_errors: listErrors,
+            is_edit: isEdit,
+        });
+    })
+    .post(upload.single("image"), async (req, res) => {
+        // Create or update SAE
+        let ressource = null;
+        const isEdit = mongoose.Types.ObjectId.isValid(req.params.id);
+        let listErrors = [];
+        let options = {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            data: {
+                ...req.body,
+                file: req.file,
+            },
+        };
+
+        if (isEdit) {
+            options = {
+                ...options,
+                method: "PUT",
+                url: `${res.locals.base_url}/api/${base}/${req.params.id}`,
+            };
+        } else {
+            options = {
+                ...options,
+                method: "POST",
+                url: `${res.locals.base_url}/api/${base}`,
+            };
+        }
+
         try {
-            result = await axios(options);
+            const result = await axios(options);
+            ressource = result.data;
         } catch (e) {
             listErrors = e.response.data.errors;
-            result = {};
+            ressource = e.response.data.ressource || {};
+        } finally {
+            if (!listErrors.length) {
+                req.flash(
+                    "success",
+                    isEdit ? "Element mis à jour" : "Element crée"
+                );
+            }
+            if (isEdit || listErrors.length) {
+                res.render("pages/back-end/saes/add-edit.njk", {
+                    sae: ressource,
+                    list_errors: listErrors,
+                    is_edit: isEdit,
+                });
+            } else {
+                res.redirect(`${res.locals.admin_url}/${base}`);
+            }
         }
-    }
-
-    res.render("pages/back-end/saes/add-edit.njk", {
-        sae: result?.data || {},
-        list_errors: listErrors,
-        is_edit: isEdit,
     });
-});
-
-// Create or update SAE 
-router.post([`/${base}/:id`, `/${base}/add`], upload.single("image"), async (req, res) => {
-    let ressource = null;
-    const isEdit = mongoose.Types.ObjectId.isValid(req.params.id)
-    let listErrors = [];
-    let options = {
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
-        data: {
-            ...req.body,
-            file: req.file,
-        },
-    }
-
-    if(isEdit) {
-        options = {
-            ...options,
-            method: "PUT",
-            url: `${res.locals.base_url}/api/${base}/${req.params.id}`,
-        }
-    } else {
-        options = {
-            ...options,
-            method: "POST",
-            url: `${res.locals.base_url}/api/${base}`,
-        }
-    }
-    
-    try {
-        const result = await axios(options);
-        ressource = result.data;
-    } catch (e) {
-        listErrors = e.response.data.errors;
-        ressource = e.response.data.ressource || {};
-    } finally {
-        if (!listErrors.length) {
-            req.flash('success', isEdit ? 'Element mis à jour' : "Element crée");
-        }
-        if (isEdit || listErrors.length) {
-            res.render("pages/back-end/saes/add-edit.njk", {
-                sae: ressource,
-                list_errors: listErrors,
-                is_edit: isEdit,
-            });
-        } else {
-            res.redirect(`${res.locals.admin_url}/${base}`);
-        }
-    }
-});
 
 export default router;
