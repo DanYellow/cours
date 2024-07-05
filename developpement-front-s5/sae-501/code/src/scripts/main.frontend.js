cdfdbb2e2839324d8d51d0a062c71069e8f555c2
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-07-05 14:38:36 +0200

    SAE-501 - Add theme-color meta tag for front-end

--- developpement-front-s5/sae-501/code/src/scripts/main.frontend.js
+++ developpement-front-s5/sae-501/code/src/scripts/main.frontend.js
@@ -3,9 +3,15 @@ import "/src/styles/hero.scss";
 import "/src/styles/index.scss";
 import "/src/styles/tailwind.css";
 
+const htmlBGColor = window
+    .getComputedStyle(document.documentElement, null)
+    .getPropertyValue("background-color");
+document
+    .querySelector('meta[name="theme-color"]')
+    .setAttribute("content", htmlBGColor);
+
 // Keep scroll position between page in navigation for mobile
 const sidebar = document.querySelector("[data-navigation]");
-
 const scrollLeft = localStorage.getItem("sidebar-scroll");
 if (scrollLeft !== null && sidebar !== null) {
     sidebar.scrollLeft = Number(scrollLeft);
