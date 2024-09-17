ff54bb760b7fb6413dde47554a42149ccb35c28c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-17 16:13:24 +0200

    developpement-front-s5@TP#5 - Move api to express server

--- developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/server.js
+++ developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/server.js
@@ -1,13 +1,55 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
-app.use(express.static('src'))
-
+app.use(express.static("src"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/src/form.html");
-}); 
+    res.sendFile(__dirname + "/src/form.html");
+});
+
+const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+app.get("/api/:region?", async (req, res) => {
+    const region = req.params.region ? req.params.region : "metropole";
+
+    const response = await fetch(
+        `https://calendrier.api.gouv.fr/jours-feries/${region}.json`
+    );
+    const jsonRes = await response.json();
+
+    const payload = [];
+    let nextDate = null;
+    const currentYear = new Date().getFullYear();
+    const endNextYear = new Date(`${currentYear + 1}-06-30`);
+
+    for (const [key, value] of Object.entries(jsonRes)) {
+        nextDate = new Date(key);
+        if (nextDate > new Date() && nextDate < endNextYear) {
+            payload.push({
+                rawDate: nextDate,
+                date: nextDate.toLocaleDateString("fr"),
+                name: value,
+            });
+        }
+    }
+
+    // Pause randomly the code
+    await new Promise((r) => setTimeout(r, randomIntFromInterval(0, 5) * 1000));
+
+    const resOrdered = payload
+        .sort((a, b) => {
+            return new Date(b.rawDate) - new Date(a.rawDate);
+        })
+        .map((item) => {
+            delete item.rawDate;
+            return item;
+        });
+
+    res.json(resOrdered);
+});
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    console.log("Server is running on http://localhost:3000");
 });
