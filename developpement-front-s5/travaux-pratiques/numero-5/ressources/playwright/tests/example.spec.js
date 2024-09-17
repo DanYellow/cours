be7ef82c8ccd94255b0e08d637fb3a8dc1402ad5
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-17 16:47:36 +0200

    developpement-front-s5@TP#5 - Use module import

--- developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/tests/example.spec.js
+++ developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/tests/example.spec.js
@@ -1,6 +1,10 @@
 // @ts-check
 import path from "path"
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 test('Visit one news from university\'s website', async ({ page }) => {
   await page.goto('https://www.cyu.fr/');
