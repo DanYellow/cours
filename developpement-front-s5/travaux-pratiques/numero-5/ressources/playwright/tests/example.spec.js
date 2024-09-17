15c8998e4c74b58041129a084061504fecdf4625
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-17 15:21:18 +0200

    developpement-front-s5@TP#5 - Update instructions

--- developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/tests/example.spec.js
+++ developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/tests/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import path from "path"
+import { test, expect } from "@playwright/test"
 
 test('Visit one news from university\'s website', async ({ page }) => {
   await page.goto('https://www.cyu.fr/');
