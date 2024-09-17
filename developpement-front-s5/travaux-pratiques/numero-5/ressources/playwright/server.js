eefc9c111421377f73e023b4df6ab1d64aec1956
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-17 16:48:48 +0200

    developpement-front-s5@TP#5 - Update instructions

--- developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/server.js
+++ developpement-front-s5/travaux-pratiques/numero-5/ressources/playwright/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(express.static("src"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/src/form.html");
