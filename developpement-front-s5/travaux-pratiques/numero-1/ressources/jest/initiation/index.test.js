581968762adced2220d132d36f58fd6918591f0c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-29 16:12:51 +0200

    developpement-front-s5@TP#1 - Update TP

--- developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.test.js
+++ developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.test.js
@@ -1,12 +1,17 @@
 import { sum, reverseString, createUsers } from "./index";
 
 test("adds 1 + 2 to equal 3 and 6 + 3 to equal 9", () => {
-  expect(sum(1, 2)).toBe(3);
-  expect(sum(6, 3)).toBe(9);
+    expect(sum(1, 2)).toBe(3);
+    expect(sum(6, 3)).toBe(9);
 });
 
 // Note : "test" can be replaced by "it"
 it("should return 4 elements", () => {
-  const nbOfItemsToCreate = 4;
-  expect(createUsers(nbOfItemsToCreate)).toHaveLength(nbOfItemsToCreate);
+    const nbOfItemsToCreate = 4;
+    expect(createUsers(nbOfItemsToCreate)).toHaveLength(nbOfItemsToCreate);
+});
+
+it('should return object with key "age"', () => {
+    const res = createUsers(2);
+    expect(res[0]).toHaveProperty("age");
 });
