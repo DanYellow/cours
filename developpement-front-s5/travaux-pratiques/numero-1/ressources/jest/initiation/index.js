c95d0b558a1cb083012313019b79b98f0457ef9e
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-09-29 15:58:41 +0200

    developpement-front-s5@TP#1 - Change folder structure

--- developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.js
+++ developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.js
@@ -1,22 +1,25 @@
+// Example function
 const sum = (val1, val2) => {
   return val1 + val2;
 };
 
-const reverseString = (str) => {
-  return str.split('').reverse().join('')
-}
-
+// Example function
 const createUsers = (nbUsers) => {
-  const minAge = 10
-  const maxAge = 65
-  const result = []
+  const minAge = 10;
+  const maxAge = 65;
+  const result = [];
 
   for (let index = 0; index < nbUsers; index++) {
     result.push({
-      age: Math.floor(Math.random() * (maxAge - minAge + 1) + minAge)
-    })
+      age: Math.floor(Math.random() * (maxAge - minAge + 1) + minAge),
+    });
   }
 
-  return result
-}
+  return result;
+};
+
+const reverseString = (str) => {
+  return str.split("").reverse().join("");
+};
+
 export { sum, reverseString, createUsers };
--- developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.test.js
+++ developpement-front-s5/travaux-pratiques/numero-1/ressources/jest/initiation/index.test.js
@@ -1,11 +1,12 @@
 import { sum, reverseString, createUsers } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
+test("adds 1 + 2 to equal 3 and 6 + 3 to equal 9", () => {
   expect(sum(1, 2)).toBe(3);
+  expect(sum(6, 3)).toBe(9);
 });
 
 // Note : "test" can be replaced by "it"
-it("returns 4 elements", () => {
+it("should return 4 elements", () => {
   const nbOfItemsToCreate = 4;
   expect(createUsers(nbOfItemsToCreate)).toHaveLength(nbOfItemsToCreate);
 });
