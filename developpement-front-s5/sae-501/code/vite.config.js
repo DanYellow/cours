9e3f3213105a18fa5cc0ec6d51b9cbd2dcca0d75
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-10-22 18:29:41 +0200

    SAE-501 - Add eslint plugin for vite

--- developpement-front-s5/sae-501/code/vite.config.js
+++ developpement-front-s5/sae-501/code/vite.config.js
@@ -1,16 +1,17 @@
 import tailwindcss from "@vituum/vite-plugin-tailwindcss";
+import eslint from "vite-plugin-eslint";
 
 export default {
-  base: "./",
-  css: {
-    // Displays the source of sass files in dev
-    devSourcemap: true,
-  },
-  plugins: [tailwindcss()],
-  appType: "custom",
-  server: {
-    // Expose the server to the network allowing access from ip address
-    host: true,
-    middlewareMode: true,
-  },
+    base: "./",
+    css: {
+        // Displays the source of sass files in dev
+        devSourcemap: true,
+    },
+    plugins: [tailwindcss(), eslint()],
+    appType: "custom",
+    server: {
+        // Expose the server to the network allowing access from ip address
+        host: true,
+        middlewareMode: true,
+    },
 };
