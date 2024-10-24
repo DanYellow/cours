10c0b5527e54811c0540cbb747c1b3b133dacd9c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-10-25 00:10:56 +0200

    SAE-501 - Remove custom eslint plugin

--- developpement-front-s5/sae-501/code/vite.config.js
+++ developpement-front-s5/sae-501/code/vite.config.js
@@ -1,14 +1,5 @@
 import tailwindcss from "@vituum/vite-plugin-tailwindcss";
 
-import eslintVite from "./eslint-vite.plugin.js";
-
-const eslintVitePlugin = eslintVite({
-    formatter: ["stylish"], // , { formatter: "json", writeOutput: true }
-    include: "**/*.js",
-    outputPath: "src/pages/back-end/debug/eslint.tmp.njk.json",
-    fix: process.env.IS_ESLINT_AUTO_FIX_ENABLED,
-});
-
 export default {
     base: "./",
     css: {
@@ -16,12 +7,12 @@ export default {
     },
     plugins: [
         tailwindcss(),
-        eslintVitePlugin,
     ],
     appType: "custom",
     server: {
         // Expose the server to the network allowing access from ip address
         host: true,
+        hmr: false,
         middlewareMode: true,
     },
 };
