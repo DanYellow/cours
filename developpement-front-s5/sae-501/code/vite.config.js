acc0f76d5d4f20185c7ee8b135fd8b29462ef74b
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-10-24 10:27:17 +0200

    SAE-501 - Add custom eslint plugin for vite

--- developpement-front-s5/sae-501/code/vite.config.js
+++ developpement-front-s5/sae-501/code/vite.config.js
@@ -1,4 +1,49 @@
 import tailwindcss from "@vituum/vite-plugin-tailwindcss";
+import { ESLint } from "eslint";
+
+const isObject = (obj) => {
+    return typeof obj === "object" && obj !== null && !Array.isArray(obj);
+};
+
+export const myPlugin = (params) => {
+    let config = {};
+
+    return {
+        name: "eslint-vite",
+        apply: "serve",
+        async configResolved(_config) {
+            config = _config;
+        },
+        async transform() {
+            const eslint = new ESLint({ fix: false });
+            const filesLinted = await eslint.lintFiles(["./src/**/*.js"]);
+
+            if (params.fix) {
+                await ESLint.outputFixes(filesLinted);
+            }
+
+            const outputPayload = {};
+
+            for (const item of params.formatter) {
+                let formatter = item;
+                if (isObject(item)) {
+                    formatter = item.formatter;
+                }
+
+                const eslintFormatter = await eslint.loadFormatter(formatter);
+                const result = eslintFormatter.format(filesLinted);
+
+                if (formatter === "stylish" || item?.getOutput === false) {
+                    console.log(result);
+                } else if (item?.getOutput === true) {
+                    outputPayload[formatter] = result;
+                }
+            }
+
+            return outputPayload;
+        },
+    };
+};
 
 export default {
     base: "./",
@@ -6,9 +51,7 @@ export default {
         // Displays the source of sass files in dev
         devSourcemap: true,
     },
-    plugins: [
-        tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
     appType: "custom",
     server: {
         // Expose the server to the network allowing access from ip address
