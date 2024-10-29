0e21790b47bcaf406135f3c6d26ed7fe9458caa0
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-10-29 13:34:44 +0100

    SAE-203 - Add robots nofollow to admin pages

--- integration-web-s2/sae-203/code/administration/ressources/includes/head.php
+++ integration-web-s2/sae-203/code/administration/ressources/includes/head.php
@@ -5,5 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./ressources/css/ne-pas-modifier/global.css">
 
+<meta name="robots" content="noindex, nofollow">
+
 <link rel="stylesheet" href="./administration/ressources/css/ne-pas-modifier/adm-style.css">
 <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
