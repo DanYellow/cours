841c395e0eb2c023abb67ba9c90e8cf4620748d4
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-03 23:42:16 +0100

    SAE-501 - Store profile bar open status in local storage

--- developpement-front-s5/sae-501/code/src/scripts/profiler-bar.js
+++ developpement-front-s5/sae-501/code/src/scripts/profiler-bar.js
@@ -1,10 +1,16 @@
 const profilerBar = document.querySelector("[data-profiler-bar]");
 const profilerBarCloseBtn = document.querySelector("[data-profiler-bar] [data-profiler-bar-close-btn]");
 const profilerBarOpenBtn = document.querySelector("[data-profiler-bar-open-btn]");
+const isProfilerBarCollapsed = localStorage.getItem("is-profiler-bar-collapsed") || false;
+
+if (JSON.parse(isProfilerBarCollapsed) === true) {
+    profilerBar.classList.add("hide");
+    profilerBarOpenBtn.classList.add("show");
+}
 
 profilerBarCloseBtn.addEventListener("click", () => {
+    localStorage.setItem("is-profiler-bar-collapsed", true);
     profilerBar.classList.add("hide");
-
     profilerBar.addEventListener("transitionend", (e) => {
         if (e.target.matches(".hide")) {
             profilerBarOpenBtn.classList.add("show");
@@ -13,6 +19,7 @@ profilerBarCloseBtn.addEventListener("click", () => {
 });
 
 profilerBarOpenBtn.addEventListener("click", () => {
+    localStorage.setItem("is-profiler-bar-collapsed", false);
     profilerBar.classList.remove("hide");
     profilerBarOpenBtn.classList.remove("show");
 });
