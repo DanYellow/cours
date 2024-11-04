a3cd6339c86a9e532db0f7dca1706f1fb0739f23
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-04 21:16:25 +0100

    SAE-501 - Update profiler middleware into a fake one

--- developpement-front-s5/sae-501/code/server/utils/profiler.middleware.js
+++ developpement-front-s5/sae-501/code/server/utils/profiler.middleware.js
@@ -1,20 +1,6 @@
 import { getNameForRoute } from "../../generate-list-routes.js";
 
-export default (req, res, next) => {
-    if (
-        process.env.NODE_ENV !== "development"
-        || req.originalUrl.includes("src")
-        || req.originalUrl.includes("@vite")
-        || req.originalUrl.includes("node_modules")
-        || req.originalUrl.includes("database")
-        || req.originalUrl.includes("styles")
-        || req.originalUrl.includes("api")
-        || req.originalUrl.includes("images")
-        || req.originalUrl.includes("fonts")
-    ) {
-        return next();
-    }
-
+export default (req, res) => {
     const routeData = getNameForRoute(res.app, req.originalUrl);
 
     req.app.locals.profiler = {
@@ -23,6 +9,4 @@ export default (req, res, next) => {
         query_string: routeData.QUERY_STRING,
         eslint_report: req.app.locals.eslint_report,
     };
-
-    next();
 };
