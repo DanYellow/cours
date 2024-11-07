19e3eb1ed0431888317de98fd3df3c476c65969a
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-07 21:59:34 +0100

    SAE-501 - Harmonize file units

--- developpement-front-s5/sae-501/code/src/scripts/back-end/preview-modal.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/preview-modal.js
@@ -20,9 +20,9 @@ function formatBytes(bytes, decimals = 2) {
 
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PiB", "EiB", "ZiB", "YiB"];
+    const sizes = ["Bytes", "kB", "MB", "GB"];
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
 
     return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 }
