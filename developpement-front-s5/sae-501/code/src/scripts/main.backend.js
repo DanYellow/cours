c2254bd13cbdcdc9a34c5589d66593bb529eb505
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-07 21:15:52 +0100

    SAE-501 - Rename delete modal module

--- developpement-front-s5/sae-501/code/src/scripts/main.backend.js
+++ developpement-front-s5/sae-501/code/src/scripts/main.backend.js
@@ -2,7 +2,7 @@ import "/src/styles/base.css";
 import "/src/styles/tailwind.css";
 import "/src/styles/back-end/index.css";
 
-import "./back-end/delete-modal.js";
+import "./back-end/delete-entry-modal.js";
 import "./back-end/preview-upload.js";
 import "./back-end/paste-clipboard-image.js";
 import "./back-end/reset-form.js";
