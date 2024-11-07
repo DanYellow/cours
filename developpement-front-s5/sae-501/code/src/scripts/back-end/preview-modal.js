6763622826fc83a2a7d0a04aac5c5b2ba50e3029
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-07 14:54:00 +0100

    SAE-501 - Display image's data in preview popup

--- developpement-front-s5/sae-501/code/src/scripts/back-end/preview-modal.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/preview-modal.js
@@ -5,8 +5,26 @@ const previewModal = document.querySelector("[data-image-preview-modal]");
 
 const imageModalContainer = previewModal.querySelector("[data-image]");
 
+const oneMo = 1024 * 1024;
+
+const getImageInfos = async (img) => {
+    const blobQuery = await fetch(img.src);
+    const blob = await blobQuery.blob();
+
+    const imgInstance = new Image();
+    imgInstance.src = img.src;
+    await imgInstance.decode();
+    console.log(imgInstance, blob)
+    return {
+        width: imgInstance.width,
+        height: imgInstance.height,
+        size: blob.size,
+        mime: blob.type,
+    };
+};
+
 listPreviewCurrentImageBtn.forEach((item) => {
-    item.addEventListener("click", (e) => {
+    item.addEventListener("click", async (e) => {
         const dataAttr = e.currentTarget.dataset.previewCurrentImageButton;
         const previewType = e.currentTarget.dataset.previewType;
         
@@ -17,6 +35,14 @@ listPreviewCurrentImageBtn.forEach((item) => {
         }
         imageModalContainer.closest("a").href = img.src;
         imageModalContainer.src = img.src;
+
+        const imgData = await getImageInfos(img);
+
+        previewModal.querySelector("[data-img-name]").textContent = imgData.type;
+        previewModal.querySelector("[data-img-dimensions]").textContent = `${imgData.width}x${imgData.height}`;
+        previewModal.querySelector("[data-img-size]").textContent = `${(imgData.size / oneMo).toFixed(2)}MB`;
+        previewModal.querySelector("[data-img-mime]").textContent = imgData.mime;
+
         previewModal.showModal();
     });
 });
