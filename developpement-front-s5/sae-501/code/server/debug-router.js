ff41fe0eca6f193b029277c7fce42e392ddcab04
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-10 00:43:58 +0100

    SAE-501 - Add route to manage todo list

--- developpement-front-s5/sae-501/code/server/debug-router.js
+++ developpement-front-s5/sae-501/code/server/debug-router.js
@@ -1,5 +1,10 @@
 import express from "express";
 import openEditor from "launch-editor";
+import fs from "fs/promises";
+import * as cheerio from "cheerio";
+import showdown from "showdown";
+
+import { JSDOM } from "jsdom";
 
 import { generateListRoutes } from "../generate-list-routes.js";
 import routeName from "#server/utils/name-route.middleware.js";
@@ -7,6 +12,11 @@ import routeName from "#server/utils/name-route.middleware.js";
 import eslintMiddleware from "./utils/eslint.middleware.js";
 
 const router = express.Router();
+
+const converter = new showdown.Converter({ 
+    tasklists: true,
+    moreStyling: true,
+});
 
 router.get("/", routeName("debug"), async (req, res) => {
     res.render("pages/back-end/debug/index.njk", {});
@@ -42,9 +52,39 @@ router.get("/eslint", eslintMiddleware, routeName("eslint"), (req, res) => {
     res.render("pages/back-end/debug/eslint.njk");
 });
 
-
 router.get("/swagger", routeName("swagger"), (req, res) => {
     res.render("pages/back-end/debug/swagger.njk");
+});
+
+router.get("/todo", async (req, res) => {
+    const file = await fs.readFile("../README.md", { encoding: "utf8" });
+    const htmlPage = converter.makeHtml(file);
+    const $ = cheerio.load(htmlPage);
+    let todolist = $("[data-todolist]").html();
+
+    // console.log(converter.makeHtml(todolist));
+    res.render("pages/back-end/debug/todo-list.njk", { todo: converter.makeHtml(todolist) });
+});
+
+router.post("/save-todo", async (req, res) => {
+    const file = await fs.readFile("../README.md", { encoding: "utf8" });
+    const htmlPage = converter.makeHtml(file);
+    const $ = cheerio.load(htmlPage);
+    // const $ = cheerio.load(req.body.html);
+    $("[data-todolist]").html(req.body.html);
+    // const foo = $("[data-todolist]").html(req.body.html);
+    // console.log($.html())
+    // console.log(
+    //     converter.makeMarkdown(
+    //         $.html(),
+    //         new JSDOM("", {}).window.document
+    //     )
+    // )
+
+    await fs.writeFile("../README.tmp.md", converter.makeMarkdown(
+        $.html(),
+        new JSDOM("", {}).window.document
+    ));
 });
 
 export default router;
