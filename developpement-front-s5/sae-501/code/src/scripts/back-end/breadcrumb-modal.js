e36635c3ac35be518862d395ab4e6cea6194c500
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-22 16:04:45 +0100

    SAE-501 - Display properly breadcrumb's modal

--- developpement-front-s5/sae-501/code/src/scripts/back-end/breadcrumb-modal.js
+++ developpement-front-s5/sae-501/code/src/scripts/back-end/breadcrumb-modal.js
@@ -10,20 +10,32 @@ listDisplayDetailsBreadcrumbBtn.forEach((item) => {
     item.addEventListener("click", async (e) => {
         modal.dataset.modal = tplId;
         const breadcrumbContentRaw = e.currentTarget.dataset.displayDetailsBreadcrumb;
-        const breadcrumbContent = breadcrumbContentRaw.split(",");
+        const breadcrumbContent = breadcrumbContentRaw.split(",").map((item) => {
+            return {
+                name: item.split("/").at(-1),
+                path: `/${item}`,
+            };
+        });
 
         const breadcrumbList = modalTemplateContent.querySelector(
             "[data-content-list]"
         );
 
+        while (breadcrumbList.firstChild) {
+            breadcrumbList.removeChild(breadcrumbList.firstChild);
+        }
+
         const breadcrumbListItemTemplate = document.querySelector(
             "[data-tpl-id='breadcrumb-item']"
         );
         
-        breadcrumbContent.forEach((breadcrumbItem) => {
+        breadcrumbContent.forEach((breadcrumbItem, idx) => {
             const breadcrumbListItemTemplateContent = breadcrumbListItemTemplate.content.cloneNode(true);
-            breadcrumbListItemTemplateContent.querySelector("a").textContent = breadcrumbItem;
+            const linkTag = breadcrumbListItemTemplateContent.querySelector("a");
+            linkTag.textContent = breadcrumbItem.name;
+            linkTag.href = breadcrumbItem.path;
 
+            breadcrumbListItemTemplateContent.querySelector("li").inert = idx === breadcrumbContent.length - 1;
             breadcrumbList.append(breadcrumbListItemTemplateContent);
         });
 
