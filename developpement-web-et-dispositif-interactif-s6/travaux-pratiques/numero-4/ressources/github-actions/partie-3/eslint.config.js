60ddae43c18a0eff648cef188fbf0e33ec2273c7
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-03 21:58:22 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Add eslint to vite

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/eslint.config.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/eslint.config.js
@@ -3,16 +3,22 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  {
-    ignores: [
-      "dist/",
-      "tests/",
-      "tailwind.config.js",
-      "vite.config.js",
-      "eslint.config.js",
-      "postcss.config.js",
-    ],
-  },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "error",
+            "no-undef": "error",
+        },
+    },
+    {
+        ignores: [
+            "dist/",
+            "tests/",
+            "tailwind.config.js",
+            "vite.config.js",
+            "eslint.config.js",
+            "postcss.config.js",
+        ],
+    },
 ];
--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/vite.config.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/vite.config.js
@@ -1,3 +1,9 @@
-export default {
-  // config options
-};
+import { defineConfig } from "vite";
+
+import eslint from "vite-plugin-eslint";
+
+export default defineConfig({
+    plugins: [eslint({
+        include: "./src/**/*.js",
+    })],
+});
