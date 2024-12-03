e5dc274c1bd051216227ead2a37b9b7ef6be8160
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-03 15:59:55 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Add close modal feature

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
@@ -5,12 +5,33 @@ const pkmnTemplateRaw = document.querySelector("[data-tpl-id='pokemon']");
 const pkdexTemplateRaw = document.querySelector("[data-tpl-id='pokedex']");
 const pokedexContainer = document.querySelector("[data-list-pokedex]");
 const loadGenerationBtn = document.querySelector("[data-load-generation]");
+const closeModalBtn = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-pokemon-modal]");
+
 
 const displayDetails = async (e) => {
-    const pkmnId = e.currentTarget.dataset.pokemonId;
+    const pkmnDataRaw = e.currentTarget.dataset.pokemonData;
+    const pkmnData = JSON.parse(pkmnDataRaw);
 
-    console.log(pkmnId);
-}
+    const imgTag = modal.querySelector("img");
+    imgTag.src = pkmnData.sprites.regular;
+    imgTag.alt = `sprite de ${pkmnData.name.fr}`;
+
+    modal.querySelector("h2").textContent = pkmnData.name.fr;
+    modal.querySelector("[data-sprite]").textContent = pkmnData.name.fr;
+
+    pkmnData.types.forEach((type) => {
+        const li = document.createElement("li");
+        li.textContent = type.name;
+        li.classList.add(...[type.name.toLowerCase(), "rounded-lg", "px-2", "py-1"])
+
+        modal.querySelector("[data-list-types]").append(li);
+    })
+
+    modal.showModal();
+
+    console.log(pkmnDataRaw);
+};
 
 const loadTemplateForGeneration = async (generation = 1) => {
     try {
@@ -18,24 +39,33 @@ const loadTemplateForGeneration = async (generation = 1) => {
         const pokedexData = await fetchPokemonForGeneration(generation);
         const cloneDex = document.importNode(pkdexTemplateRaw.content, true);
         const pokedex = cloneDex.querySelector("[data-pokedex]");
-        const generationNumber = cloneDex.querySelector("[data-generation-number]");
-        const generationRange = cloneDex.querySelector("[data-generation-range]");
+        const generationNumber = cloneDex.querySelector(
+            "[data-generation-number]"
+        );
+        const generationRange = cloneDex.querySelector(
+            "[data-generation-range]"
+        );
 
         generationNumber.textContent = `#${generation}`;
-        generationRange.textContent = `${pokedexData[0].pokedex_id} -> ${pokedexData.at(-1).pokedex_id}`;
+        generationRange.textContent = `${pokedexData[0].pokedex_id} -> ${
+            pokedexData.at(-1).pokedex_id
+        }`;
 
         pokedexData.forEach((item) => {
             const clone = document.importNode(pkmnTemplateRaw.content, true);
-            clone.querySelector("img").src = item.sprites.regular;
-            clone.querySelector("img").alt = `sprite de ${item.name.fr}`;
-            clone.querySelector("figcaption").textContent = `#${item.pokedex_id} ${item.name.fr}`;
-        
-            const button = clone.querySelector("[data-pokemon-id]");
-            button.dataset.pokemonId = item.pokedex_id;
+            const imgTag = clone.querySelector("img");
+            imgTag.src = item.sprites.regular;
+            imgTag.alt = `sprite de ${item.name.fr}`;
+            clone.querySelector(
+                "figcaption"
+            ).textContent = `#${item.pokedex_id} ${item.name.fr}`;
+
+            const button = clone.querySelector("[data-pokemon-data]");
+            button.dataset.pokemonData = JSON.stringify(item);
             button.addEventListener("click", displayDetails);
-        
+
             pokedex.append(clone);
-        })
+        });
         loadGenerationBtn.dataset.loadGeneration = Number(generation) + 1;
 
         pokedexContainer.append(cloneDex);
@@ -43,12 +73,14 @@ const loadTemplateForGeneration = async (generation = 1) => {
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 loadTemplateForGeneration();
 
 loadGenerationBtn.addEventListener("click", (e) => {
-    loadTemplateForGeneration(e.currentTarget.dataset.loadGeneration)
-})
+    loadTemplateForGeneration(e.currentTarget.dataset.loadGeneration);
+});
 
-
+closeModalBtn.addEventListener("click", () => {
+    modal.close();
+});
