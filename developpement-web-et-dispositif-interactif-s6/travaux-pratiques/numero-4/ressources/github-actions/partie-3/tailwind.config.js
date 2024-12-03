97176365ae2d22b08edbc31d04b6334757dbcde8
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-03 16:04:25 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - wip - classes for type

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/tailwind.config.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/tailwind.config.js
@@ -4,13 +4,51 @@ import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./index.html", "./src/**/*.{js}"],
+  safelist: ["plante", "poison", "feu", "eau", "vol", "insecte", "normal", "sol"],
   theme: {
-    extend: {},
+    extend: {
+        transitionProperty: {
+            "dialog": "background-color box-shadow"
+        },
+    },
   },
   plugins: [
-    plugin(({ addVariant }) => {
+    plugin(({ addVariant, addComponents, theme }) => {
         addVariant("inert", "&:where([inert], [inert] *)");
         addVariant("hocus", ["&:hover", "&:focus-visible"]);
+        addComponents({
+            ".plante": {
+                backgroundColor: theme("colors.green.500"),
+            },
+            ".poison": {
+                backgroundColor: theme("colors.violet.500"),
+            },
+            ".vol": {
+                backgroundColor: theme("colors.violet.500"),
+            },
+            ".eau": {
+                backgroundColor: theme("colors.blue.500"),
+            },
+            ".feu": {
+                backgroundColor: theme("colors.red.500"),
+            },
+            ".normal": {
+                backgroundColor: theme("colors.zinc.100"),
+            },
+            ".insecte": {
+                backgroundColor: theme("colors.red.500"),
+            },
+            ".sol": {
+                backgroundColor: theme("colors.amber.500"),
+            },
+            ".tenebres": {
+                backgroundColor: theme("colors.gray.900"),
+                color: theme("colors.white"),
+            },
+            ".psy": {
+                backgroundColor: theme("colors.pink.500"),
+            },
+        });
     }),
   ],
 }
