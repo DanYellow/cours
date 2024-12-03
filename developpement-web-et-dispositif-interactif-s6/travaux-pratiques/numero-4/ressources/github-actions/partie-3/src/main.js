8a9c7f3fb2939621a6f26aa6a0f88affff5468df
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-03 15:17:18 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Manage loading of next generation

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
@@ -4,6 +4,7 @@ import fetchPokemonForGeneration from "./api";
 const pkmnTemplateRaw = document.querySelector("[data-tpl-id='pokemon']");
 const pkdexTemplateRaw = document.querySelector("[data-tpl-id='pokedex']");
 const pokedexContainer = document.querySelector("[data-list-pokedex]");
+const loadGenerationBtn = document.querySelector("[data-load-generation]");
 
 const displayDetails = async (e) => {
     const pkmnId = e.currentTarget.dataset.pokemonId;
@@ -16,6 +17,12 @@ const loadTemplateForGeneration = async (generation = 1) => {
         const pokedexData = await fetchPokemonForGeneration(generation);
         const cloneDex = document.importNode(pkdexTemplateRaw.content, true);
         const pokedex = cloneDex.querySelector("[data-pokedex]");
+        const generationNumber = cloneDex.querySelector("[data-generation-number]");
+        const generationRange = cloneDex.querySelector("[data-generation-range]");
+
+        generationNumber.textContent = `#${generation}`;
+        generationRange.textContent = `${pokedexData[0].pokedex_id} -> ${pokedexData.at(-1).pokedex_id}`;
+
         pokedexData.forEach((item) => {
             const clone = document.importNode(pkmnTemplateRaw.content, true);
             clone.querySelector("img").src = item.sprites.regular;
@@ -28,13 +35,18 @@ const loadTemplateForGeneration = async (generation = 1) => {
         
             pokedex.append(clone);
         })
+        loadGenerationBtn.dataset.loadGeneration = Number(generation) + 1;
+
         pokedexContainer.append(cloneDex);
     } catch (error) {
         console.log(error);
     }
-    
 }
 
 loadTemplateForGeneration();
+
+loadGenerationBtn.addEventListener("click", (e) => {
+    loadTemplateForGeneration(e.currentTarget.dataset.loadGeneration)
+})
 
 
