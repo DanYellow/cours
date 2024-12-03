1027bb16d6e88e91fe361dc0413ea7d5fc6cd30f
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-03 15:08:54 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Refactoring pokedex management

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/api.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/api.js
@@ -9,15 +9,14 @@ const fetchPokemon = async (url) => {
     }
 }
 
+export { fetchPokemon };
+
 const fetchListPokemon = async (generation = 1) => {
     let listPokemon = [];
     try {
         const req = await axios.get(`https://tyradex.vercel.app/api/v1/gen/${generation}`);
-        // for (const pkmn of req.data.results) {
-        //     const res = await fetchPokemon(pkmn.url);
-        //     listPokemon.push(res);
-        // }
         listPokemon = req.data;
+
         return listPokemon;
     } catch (error) {
         throw new Error(error);
--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/src/main.js
@@ -1,17 +1,40 @@
 import "./style.css";
-import fetchPokemon from "./api";
+import fetchPokemonForGeneration from "./api";
 
 const pkmnTemplateRaw = document.querySelector("[data-tpl-id='pokemon']");
-const pokedexContainer = document.querySelector("[data-pokedex]");
+const pkdexTemplateRaw = document.querySelector("[data-tpl-id='pokedex']");
+const pokedexContainer = document.querySelector("[data-list-pokedex]");
 
-const pokedex = await fetchPokemon();
+const displayDetails = async (e) => {
+    const pkmnId = e.currentTarget.dataset.pokemonId;
 
-pokedex.forEach((item) => {
-    console.log(item)
-    const clone = document.importNode(pkmnTemplateRaw.content, true);
-    clone.querySelector("img").src = item.sprites.regular;
-    clone.querySelector("img").alt = `sprite de ${item.name.fr}`;
-    clone.querySelector("figcaption").textContent = `#${item.pokedex_id} ${item.name.fr}`;
+    console.log(pkmnId);
+}
 
-    pokedexContainer.append(clone);
-})
+const loadTemplateForGeneration = async (generation = 1) => {
+    try {
+        const pokedexData = await fetchPokemonForGeneration(generation);
+        const cloneDex = document.importNode(pkdexTemplateRaw.content, true);
+        const pokedex = cloneDex.querySelector("[data-pokedex]");
+        pokedexData.forEach((item) => {
+            const clone = document.importNode(pkmnTemplateRaw.content, true);
+            clone.querySelector("img").src = item.sprites.regular;
+            clone.querySelector("img").alt = `sprite de ${item.name.fr}`;
+            clone.querySelector("figcaption").textContent = `#${item.pokedex_id} ${item.name.fr}`;
+        
+            const button = clone.querySelector("[data-pokemon-id]");
+            button.dataset.pokemonId = item.pokedex_id;
+            button.addEventListener("click", displayDetails);
+        
+            pokedex.append(clone);
+        })
+        pokedexContainer.append(cloneDex);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+loadTemplateForGeneration();
+
+
