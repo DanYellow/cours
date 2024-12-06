dba1190c0e56163dc9942df47c8227cd4e22a5b8
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-06 10:33:00 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Update e2e tests

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/e2e/modal.spec.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/e2e/modal.spec.js
@@ -3,9 +3,11 @@ import { test, expect } from "@playwright/test";
 test("should open modal", async ({ page }) => {
     await page.goto("http://localhost:5173");
     const firstPkmn = await page.getByTestId("pokemon").first();
-    const firstPkmnData = await page.evaluate(() => document.querySelectorAll('[data-pokemon-data]'))
-    console.log(firstPkmnData)
+    const firstPkmnDataRaw = await firstPkmn.getAttribute("data-pokemon-data");
+    const firstPkmnData = JSON.parse(firstPkmnDataRaw);
     firstPkmn.click();
     await expect(page.getByTestId("pokemon-modal")).toHaveAttribute("open");
-    //   await expect(page).toHaveTitle(/Playwright/);
+    await expect(page).toHaveTitle(
+        new RegExp(String.raw`\s${firstPkmnData.name.fr}\s`, "g")
+    );
 });
