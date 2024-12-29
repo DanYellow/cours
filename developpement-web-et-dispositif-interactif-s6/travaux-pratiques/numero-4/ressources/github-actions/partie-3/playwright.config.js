429c2ccbc6c5ed915d42b7664a1eaa60d38050f3
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-12-30 00:06:08 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Update instructions

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/playwright.config.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/playwright.config.js
@@ -6,6 +6,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
+const baseURL = 'http://localhost:5173'
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -20,11 +22,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -71,6 +73,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
   },
 });
