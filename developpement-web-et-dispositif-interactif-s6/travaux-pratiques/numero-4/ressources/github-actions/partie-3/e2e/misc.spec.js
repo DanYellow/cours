9b1cf146f9b95fd8e1b0e260dd691f921558d1eb
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-01-20 00:44:27 +0100

    developpement-web-et-dispositif-interactif-s6@TP#4 - Add layout e2e tests

--- developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/e2e/misc.spec.js
+++ developpement-web-et-dispositif-interactif-s6/travaux-pratiques/numero-4/ressources/github-actions/partie-3/e2e/misc.spec.js
@@ -7,16 +7,25 @@ test("should set list layout", async ({ page }) => {
     await firstLayoutSwitch.waitFor();
     await firstLayoutSwitch.click();
 
-    const sessionStorage = await page.evaluate(() => {
-        console.log(JSON.stringify(window.localStorage));
-    });
-    // console.log(sessionStorage);
-    // await page.waitForTimeout(2000);
-    // await page.context().storageState({ path: "./user.tmp.json" });
+    const isGridLayout = await page.evaluate(async () =>
+        localStorage.getItem("is_grid_layout")
+    );
+
+    await expect(JSON.parse(isGridLayout)).toBeFalsy();
+});
+
+test("should set grid layout", async ({ page }) => {
+    await page.goto("/");
+
+    const firstLayoutSwitch = await page.getByTestId("switch-layout").first();
+    await firstLayoutSwitch.waitFor();
+    await firstLayoutSwitch.click();
+    await firstLayoutSwitch.click();
 
     const isGridLayout = await page.evaluate(async () =>
         localStorage.getItem("is_grid_layout")
     );
 
-    await expect(isGridLayout).toBeFalsy();
+    await expect(JSON.parse(isGridLayout)).toBeTruthy();
 });
+
