142c32a6565d7586573ea676f28bf1284f132cd6
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-03-01 22:00:12 +0100

    developpement-front-s5@SAE-501 - Continue tailwind upgrade

--- developpement-front-s5/sae-501/code/tailwind.config.js
+++ developpement-front-s5/sae-501/code/tailwind.config.js
@@ -1,37 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 import forms from "@tailwindcss/forms";
-import plugin from "tailwindcss/plugin";
 
 export default {
     content: ["./src/**/*.{js,ts,jsx,tsx,njk}"],
-    theme: {
-        extend: {
-            spacing: {
-                11: "2.75rem",
-                12: "3.25rem",
-                13: "3.75rem",
-                14: "4.25rem",
-            },
-            containers: {
-                "5.5xl": "69rem",
-            },
-        },
-    },
     plugins: [
         forms,
-        // plugin(({ addVariant, addComponents, theme }) => {
-        //     addVariant("touch", "@media (pointer: coarse)");
-        //     addVariant("no-touch", "@media (pointer: fine)");
-        //     addVariant("inert", "&:where([inert], [inert] *)");
-        //     addVariant("hocus", ["&:hover", "&:focus-visible"]);
-        //     addVariant('group-hocus', [':merge(.group):hover &', ':merge(.group):focus-visible &']);
-        //     addVariant("starting", "@starting-style");
-        //     addComponents({
-        //         ".active-tab": {
-        //             backgroundColor: theme("colors.slate.100"),
-        //         },
-        //     });
-        // }),
     ],
 };
