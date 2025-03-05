7ca3ade474f8a8540caae01fca38f39e6eef2da1
Danielo Jean-Louis (DanYellow) <jeanlouis.danielo@sfr.fr>
2025-03-05 10:17:11 +0100

    integration-web-s2@SAE-203 - Update navigation in administration

--- integration-web-s2/sae-203/code/administration/ressources/includes/menu-principal.php
+++ integration-web-s2/sae-203/code/administration/ressources/includes/menu-principal.php
@@ -26,30 +26,32 @@ $liste_entrees_menu = [
 
 <nav class="bg-gradient-to-r from-gray-800 to-slate-900">
     <div class="mx-auto max-w-7xl px-4">
-        <div class="flex h-16 items-center justify-between">
-            <div class="flex items-center">
-                <div class="flex-shrink-0">
-                    <p class="text-white font-bold">
-                        <a href="/admin">Administration SAE 203</a>
-                    </p>
-                </div>
-                    <nav class="ml-10 flex items-baseline space-x-4">
-                        <?php foreach ($liste_entrees_menu as $entree_menu) {
-                            $liste_classes = 'text-white ';
-                            $aria_current_attr = "";
-                            if ($page_courante === $entree_menu["clef"]) {
-                                $aria_current_attr = "aria-current='page'";
-                                $liste_classes = "bg-white text-gray-900";
-                            }
-            
-                            echo "
-                                <a href='{$entree_menu["lien"]}' class='{$liste_classes} rounded-md font-medium hover:bg-gray-700 hover:text-white focus-within:bg-gray-700 focus-within:text-white px-3 py-2' $aria_current_attr>
-                                    {$entree_menu["nom"]}
-                                </a>
-                            ";
-                        } ?>
-                    </nav>
+        <div class="flex h-16 items-center">
+            <div class="flex-shrink-0">
+                <p class="text-white font-bold">
+                    <a href="./administration/articles">Administration SAE 203</a>
+                </p>
             </div>
+            <nav class="ml-10 flex items-baseline space-x-4 grow">
+                <?php 
+                    foreach ($liste_entrees_menu as $key => $entree_menu) {
+                        $liste_classes = 'text-white';
+                        if ($key === array_key_last($liste_entrees_menu)) {
+                            $liste_classes .= ' ml-auto';
+                        }
+                        $aria_current_attr = "";
+                        if ($page_courante === $entree_menu["clef"]) {
+                            $aria_current_attr = "aria-current='page'";
+                            $liste_classes = "bg-white text-gray-900";
+                        }
+    
+                        echo "
+                            <a href='{$entree_menu["lien"]}' class='{$liste_classes} rounded-md font-medium hover:bg-gray-700 hover:text-white focus-within:bg-gray-700 focus-within:text-white px-3 py-2' $aria_current_attr>
+                                {$entree_menu["nom"]}
+                            </a>
+                        ";
+                } ?>
+            </nav>
         </div>
     </div>
 </nav>
