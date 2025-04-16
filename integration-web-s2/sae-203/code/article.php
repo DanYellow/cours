c4d60fb0759f928f539f45ec3be68d0c536d372c
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-04-16 19:55:06 +0200

    integration-web-s2@SAE-203 - Update footer in front-end

--- integration-web-s2/sae-203/code/article.php
+++ integration-web-s2/sae-203/code/article.php
@@ -7,7 +7,7 @@ require_once('./ressources/includes/connexion-bdd.php');
 // Code à améliorer
 $id = 10;
 $requete_brute = "
-    SELECT * FROM article 
+    SELECT * FROM article
     WHERE article.id = $id
 ";
 $resultat_brut = mysqli_query($mysqli_link, $requete_brute);
@@ -31,6 +31,17 @@ $entite = mysqli_fetch_array($resultat_brut);
 
     <link rel="stylesheet" href="./ressources/css/global.css">
     <link rel="stylesheet" href="./ressources/css/accueil.css">
+
+    <!-- A supprimer -->
+    <style>
+        .no-design {
+            padding: 0.5rem 1rem;
+            background-color: lightgray;
+            border: 1px solid gray;
+            border-radius: 0.25rem;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -44,7 +55,7 @@ $entite = mysqli_fetch_array($resultat_brut);
     <!-- Vous allez principalement écrire votre code HTML ci-dessous -->
     <main class="conteneur-principal conteneur-1280">
         <h1 class="titre"><?php echo $entite["titre"]; ?></h1>
-        <p>A vous de faire le design de l'article</p>
+        <p class="no-design">A vous de faire le design de l'article</p>
     </main>
     <?php require_once('./ressources/includes/footer.php'); ?>
 </body>
--- integration-web-s2/sae-203/code/ressources/includes/footer.php
+++ integration-web-s2/sae-203/code/ressources/includes/footer.php
@@ -10,7 +10,7 @@
             <li>
             <!--
                 Tous les lecteurs d'écran ne lisent pas forcément le contenu de l'attribut "title", c'est pourquoi nous pouvons
-                utiliser l'attribut "aria-label" qui doit s'utiliser QUE quand le libellé n'est pas lisible. 
+                utiliser l'attribut "aria-label" qui doit s'utiliser QUE quand le libellé n'est pas lisible.
                 A noter que l'attribut "aria-label" n'est lu QUE par les lecteurs d'écran, il n'est donc pas redondant de mettre l'attribut "title" et "aria-label"
 
                 Plus d'informations ici : https://developer.mozilla.org/fr/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_aria-label_attribute
@@ -25,7 +25,7 @@
             </li>
             <!--
                 L'attribut "aria-hidden" permet d'empêcher les lecteurs d'écran de lire un élément
-                Ici le caractère • n'est là que pour décorer, il est donc inutile pour les lecteurs d'écran 
+                Ici le caractère | n'est là que pour décorer, il est donc inutile pour les lecteurs d'écran
                 Plus d'informations ici : https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden - anglais
                 ou encore ici : https://www.a11y-101.com/development/aria-hidden - anglais
             -->
@@ -37,8 +37,8 @@
             </li>
         </ul>
 
-        <p class="txt-credits">SAE 203 • © 2021-<?php echo date("Y"); ?> - BUT MMI - CY Paris Université - Site de Sarcelles</p>
+        <p class="txt-credits">SAE 203 • BUT MMI - CY Paris Université - Site de Sarcelles</p>
     </section>
 
-    
+
 </footer>
