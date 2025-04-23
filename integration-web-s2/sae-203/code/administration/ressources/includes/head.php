22d2405ee6ef82c5ae344c9232609a56fef4bcc5
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-04-23 14:24:27 +0200

    integration-web-s2@SAE-203 - Use DMSans as font for administration

--- integration-web-s2/sae-203/code/administration/ressources/includes/head.php
+++ integration-web-s2/sae-203/code/administration/ressources/includes/head.php
@@ -3,7 +3,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./ressources/css/ne-pas-modifier/global.css">
+<link rel="stylesheet" href="./ressources/css/ne-pas-modifier/fonts.css">
 
 <meta name="robots" content="noindex, nofollow">
 
