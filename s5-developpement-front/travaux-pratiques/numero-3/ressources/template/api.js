78ceeb41c795c78d3a160cdc6efb0de95a35f1f5
PC-Dan <jeanlouis.danielo@sfr.fr>
2025-08-20 14:12:57 +0200

    s5-developpement-front@TP#3 - Update instructions

--- s5-developpement-front/travaux-pratiques/numero-3/ressources/template/api.js
+++ s5-developpement-front/travaux-pratiques/numero-3/ressources/template/api.js
@@ -1,20 +1,23 @@
 const getListNationalHolidays = async (region = "metropole") => {
-  const response = await fetch(
-    `https://calendrier.api.gouv.fr/jours-feries/${region}.json`, 
-  );
-  const jsonRes = await response.json();
+    try {
+        const response = await fetch(
+            `https://calendrier.api.gouv.fr/jours-feries/${region}/${new Date().getFullYear()}.json`
+        );
+        const jsonRes = await response.json();
 
-  const res = [];
-  let nextDate = null;
+        const res = [];
+        let nextDate = null;
 
-  for (const [key, value] of Object.entries(jsonRes)) {
-    nextDate = new Date(key);
-    res.push({
-      date: nextDate.toLocaleDateString("fr"),
-      name: value,
-    });
-  }
+        for (const [key, value] of Object.entries(jsonRes)) {
+            nextDate = new Date(key);
+            res.push({
+                date: nextDate.toLocaleDateString("fr"),
+                name: value,
+            });
+        }
 
-  return res;
+        return res;
+    } catch (error) {
+        return {};
+    }
 };
-
