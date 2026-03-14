5673052396eea127c57f1167b65f1b0915c5c105
PC-Dan <jeanlouis.danielo@sfr.fr>
2026-03-14 21:55:03 +0100

    s4-creation-et-design-interactif@TP#1 - Update EventChannel abstract class

--- s4-creation-et-design-interactif/travaux-pratiques/numero-1/ressources/unity/ScriptableObjects/Templates/Events/EventChannel.cs
+++ s4-creation-et-design-interactif/travaux-pratiques/numero-1/ressources/unity/ScriptableObjects/Templates/Events/EventChannel.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+public abstract class EventChannel : ScriptableObject
+{
+    public UnityAction OnEventRaised;
+
+    public void Raise()
+    {
+        OnEventRaised?.Invoke();
+    }
+}
+
 public abstract class EventChannel<T> : ScriptableObject
 {
     public UnityAction<T> OnEventRaised;
